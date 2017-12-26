4b035d7c434ec33c3bc6d22fe81039d071d9fb0e
BikashG12 <bikash57@live>
2017-12-26 13:32:15 +0545

    Splash Screen,Internet Check,Notification pull down/up re-check conntection,Alert Dialog Theme change

--- app/src/main/java/com/infobrain/meroticket/Activities/SplashSceen.java
+++ app/src/main/java/com/infobrain/meroticket/Activities/SplashSceen.java
@@ -1,11 +1,19 @@
 package com.infobrain.meroticket.Activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,9 +21,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.infobrain.meroticket.Networks.CheckConnection;
 import com.infobrain.meroticket.R;
 import com.infobrain.meroticket.SqliteDB.DBHelper;
 import com.infobrain.meroticket.SqliteDB.SQLiteOperations;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,18 +42,63 @@ public class SplashSceen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_sceen);
+
+
         dbObject = new DBHelper(this);
         URL = "https://laxmicapital.com.np/abc/services/webservice.asmx/Get_City_List?C_Code=1001";
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                SaveCityList();
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashSceen.this, MainActivity.class);
-                SplashSceen.this.startActivity(mainIntent);
-                SplashSceen.this.finish();
+                checkInternetCon();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+
+    public void checkInternetCon() {
+        if (!CheckConnection.checkInternetConnection(this)) {
+            AlertMessage();
+            //Toast.makeText(getApplicationContext(), "No Internet!", Toast.LENGTH_SHORT).show();
+        } else {
+            SaveCityList();
+            Intent mainIntent = new Intent(SplashSceen.this, MainActivity.class);
+            SplashSceen.this.startActivity(mainIntent);
+            SplashSceen.this.finish();
+
+        }
+    }
+
+    public void AlertMessage() {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Light_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("No Internet Connection");
+        builder.setCancelable(false)
+                .setPositiveButton(R.string.TryAgain, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        checkInternetCon();
+                    }
+                })
+                .setNegativeButton(R.string.Exit, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            // notification is pulled up
+        } else {
+            checkInternetCon();
+        }
+        super.onWindowFocusChanged(hasFocus);
     }
 
 
---
+++ app/src/main/java/com/infobrain/meroticket/Networks/CheckConnection.java
@@ -0,0 +1,22 @@
+package com.infobrain.meroticket.Networks;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+/**
+ * Created by bikas on 12/26/2017.
+ */
+
+public class CheckConnection {
+
+    public static boolean checkInternetConnection(Context context) {
+
+        ConnectivityManager con_manager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return (con_manager.getActiveNetworkInfo() != null
+                && con_manager.getActiveNetworkInfo().isAvailable()
+                && con_manager.getActiveNetworkInfo().isConnected());
+    }
+}
+
